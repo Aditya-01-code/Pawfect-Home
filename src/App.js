69be26f7e4f6baf846f53e6c2bfbb57534f6ce01
Aditya-01-code <adityaraj0878@gmail.com>
2025-08-04 21:08:53 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Sections for Home
+import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import InfoSection from "./components/InfoSection";
+import GallerySection from "./components/GallerySection";
+import FeaturesSection from "./components/FeaturesSection";
+import ContactSection from "./components/ContactSection";
+import Footer from "./components/Footer";
+
+// Separate Pages
+import FeaturePage from "./pages/FeaturePage";
+import GalleryPage from "./pages/GalleryPage";
+import AdoptPage from "./pages/AdoptPage";
+import ContactPage from "./pages/ContactPage";
+
+const HomePage = () => (
+  <>
+    <Navbar />
+    <HeroSection />
+    <InfoSection />
+    <GallerySection />
+    <FeaturesSection />
+    <ContactSection />
+    <Footer />
+  </>
+);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/features" element={<FeaturePage />} />
+        <Route path="/gallery" element={<GalleryPage />} />
+        <Route path="/adopt" element={<AdoptPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+      </Routes>
+    </Router>
   );
 }
 
